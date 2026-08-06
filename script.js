0b34863fd1d663466ef8a2007dbc1d98d07db7fe
yoyashcsk-sys <yoyash.csk@gmail.com>
2026-08-06 14:01:33 +0530

    Enhance transition and update scene change timing

--- script.js
+++ script.js
@@ -3,16 +3,43 @@ const btn = document.getElementById("startBtn");
 
 btn.addEventListener("click",()=>{
 
-document.body.style.transition="1s";
-
+document.body.style.transition="all 1.2s ease";
 document.body.style.opacity="0";
 
 setTimeout(()=>{
 
-alert("Scene 2 Coming Soon ❤️");
+document.body.innerHTML=`
+
+<div class="container">
+
+<h1 style="font-size:45px;">Dear ❤️</h1>
+
+<p id="typing"></p>
+
+</div>
+
+`;
+
+const text="Before you read this... There is something I truly need to tell you.";
+
+let i=0;
+
+const typing=setInterval(()=>{
+
+document.getElementById("typing").innerHTML+=text.charAt(i);
+
+i++;
+
+if(i>=text.length){
+
+clearInterval(typing);
+
+}
+
+},55);
 
 document.body.style.opacity="1";
 
-},1000);
+},1200);
 
 });
