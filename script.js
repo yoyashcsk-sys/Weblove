d12e4a9e2c98cd48b4bb8e8aec0ef967547e81ed
yoyashcsk-sys <yoyash.csk@gmail.com>
2026-08-06 13:48:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
-document.getElementById("startBtn").onclick = () => {
+const btn = document.getElementById("startBtn");
 
-    alert("The story begins... ❤️");
+btn.addEventListener("click",()=>{
 
-};
+document.body.style.transition="1s";
+
+document.body.style.opacity="0";
+
+setTimeout(()=>{
+
+alert("Scene 2 Coming Soon ❤️");
+
+document.body.style.opacity="1";
+
+},1000);
+
+});
